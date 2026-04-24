da11cb0b397b6109c00a6dd067565b9c77ffd092
Natalie Huynh <n3huynh@ucsd.edu>
2026-04-24 14:22:54 -0700

    add count

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,12 @@
-//adding for step 1.3
 import { fetchJSON, renderProjects } from '../global.js';
+
 const projects = await fetchJSON('../lib/projects.json');
+
 const projectsContainer = document.querySelector('.projects');
+const titleElement = document.querySelector('.projects-title');
+
+// update title with count
+titleElement.textContent = `Projects (${projects.length})`;
+
+// render projects
 renderProjects(projects, projectsContainer, 'h2');
