1f908f2d56bd8f3da4d52be741dccdaef9e33f97
Natalie Huynh <n3huynh@ucsd.edu>
2026-05-08 08:18:34 -0700

    final push

--- meta/main.js
+++ meta/main.js
@@ -236,6 +236,7 @@ function brushed(event) {
     isCommitSelected(selection, d),
   );
   renderSelectionCount(selection)
+  renderLanguageBreakdown(selection)
 }
 
 function isCommitSelected(selection, commit) {
@@ -264,6 +265,39 @@ function renderSelectionCount(selection) {
   return selectedCommits;
 }
 
+function renderLanguageBreakdown(selection) {
+  const selectedCommits = selection
+    ? commits.filter((d) => isCommitSelected(selection, d))
+    : [];
+  const container = document.getElementById('language-breakdown');
+
+  if (selectedCommits.length === 0) {
+    container.innerHTML = '';
+    return;
+  }
+  const requiredCommits = selectedCommits.length ? selectedCommits : commits;
+  const lines = requiredCommits.flatMap((d) => d.lines);
+
+  // Use d3.rollup to count lines per language
+  const breakdown = d3.rollup(
+    lines,
+    (v) => v.length,
+    (d) => d.type,
+  );
+
+  // Update DOM with breakdown
+  container.innerHTML = '';
+
+  for (const [language, count] of breakdown) {
+    const proportion = count / lines.length;
+    const formatted = d3.format('.1~%')(proportion);
+
+    container.innerHTML += `
+            <dt>${language}</dt>
+            <dd>${count} lines (${formatted})</dd>
+        `;
+  }
+}
 
 let data = await loadData();
 commits = processCommits(data);
